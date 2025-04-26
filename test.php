9470ea17de9f7813f37d5a1c609ecc711c0bbf30
baselAhmed2 <baselahmed363@gmail.com>
2025-04-26 17:55:39 +0300

    رسالة توضح التغييرات التي قمت بها

---
+++ test.php
@@ -0,0 +1,36 @@
+<?php
+// // معلومات الاتصال بقاعدة البيانات
+
+// $conn = new mysqli("localhost", "root", "", "kemet_database");
+
+
+
+// if ($conn->connect_error) {
+//     die("فشل الاتصال: " . $conn->connect_error);
+// }
+
+
+// $sql = "SELECT * FROM product";
+// $result = $conn->query($sql);
+
+// if ($result->num_rows > 0) {
+//     // إخراج البيانات لكل صف
+//     while($row = $result->fetch_assoc()) {
+//         echo "الاسم: " . $row["اسم_العمود"] . "<br>";
+//         // معالجة باقي البيانات حسب الحاجة
+//     }
+// } else {
+//     echo "0 نتائج";
+// }
+
+// // إغلاق الاتصال
+// $conn->close();
+
+
+
+
+
+
+
+
+?>
