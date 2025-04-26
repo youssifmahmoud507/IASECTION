0043f58aca4a328d38c1c289ddc3be561c65c0b8
youssifmahmoud507 <mahmoudy060@gmail.com>
2025-04-26 16:10:54 +0300

    project

---
+++ Kemet/sign-connection.php
@@ -0,0 +1,59 @@
+<?php
+if(!isset($_SESSION)){
+    session_start();
+}
+
+
+
+$db=mysqli_connect('localhost','root','','project-ia');
+$error=array();
+
+if(isset($_POST['signup'])){
+    
+// mysqli_real_escape_string make sure  that the string didnot lose any letter
+$username=mysqli_real_escape_string($db,$_POST['name']);
+$email=mysqli_real_escape_string($db,$_POST['email']);
+$pass=mysqli_real_escape_string($db,$_POST['password']);
+
+
+if(empty($username)){
+     
+    array_push($error,"username is required");
+}
+
+if(empty($email)){
+          
+    array_push($error,"Email is required");
+}
+if(empty($pass)){
+       
+    array_push($error,"password is required");
+}
+
+if(count($error)==0){
+    // user is the name of table in the database that i want to insert the data into it
+    // values is the name of variables i created here
+    // (user_name,email,password) name of columns in the table
+    $sql="INSERT INTO signup (name,email,password) values('$username','$email','$pass')";
+    mysqli_query($db,$sql);
+                   
+
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ Kemet/signup.php
@@ -0,0 +1,111 @@
+<?php
+include('sign-connection.php')
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>sign up</title>
+
+    <!-- link google fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Bree+Serif&family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Signika+Negative:wght@300..700&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
+        rel="stylesheet">
+
+    <!-- link font awesome icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css"
+        integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet" href="./css/signup.css">
+</head>
+
+<body>
+
+    <!-- navbar -->
+
+    <nav class="navbar">
+
+        <div class="logo">
+            <img src="img/kemet logo.jpg" alt="logo">
+            <h1>
+                Kemet
+            </h1>
+        </div>
+
+        <div class="list">
+            <ul>
+                <a href="home page.html">
+                    <li>
+                        Home
+                    </li>
+                </a>
+                <a href="home page.html #categories">
+                    <li>
+                        Categories
+                    </li>
+                </a>
+                <a href="about_us.html">
+                    <li>
+                        About
+                    </li>
+                </a>
+                <a href="contact.html">
+                    <li>
+                        Contact Us
+                    </li>
+                </a>
+                <a href="#">
+                    <li>
+                        <i class="fa-solid fa-heart"></i>
+                    </li>
+                </a>
+                <a href="#">
+                    <li>
+                        <i class="fa-solid fa-cart-shopping"></i>
+                    </li>
+                </a>
+
+            </ul>
+        </div>
+    </nav>
+
+
+    <!-- end navbar -->
+
+    <div class="main">
+        <div class="sign">
+            <h1>Sign Up</h1>
+
+            <form class="form" method="post">
+                <label for="name">Name</label><br>
+                <input type="text" id="name" name="name" required>
+                <br>
+                <label for="email">Email</label><br>
+                <input type="email" id="email" name="email" required>
+                <br>
+                <label for="password">Password</label>
+                <br>
+                <input type="password" id="password" name="password" required>      
+                <br>
+                <label for="confirm"> Confirm Password</label>
+                <br>
+                <input type="password" id="confirm" name="confirm-Password" required>
+                <br>
+                <input type="submit" value="Signup" id="submit" name="signup">
+            </form>
+
+            <div class="x">
+                <p>Already have an account ? <a href="login.html">log in</a> </p>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
